fff3ff4d2462e6ff6181099e5dd4bb43523abf27
wisdom-maduchor <maduchorwisdom@gmail.com>
2026-01-29 23:45:53 +0100

    Finalize Branding and Deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
